627d55a772053bb0a64e8b62b90035d0f9a9fa5a
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-01-30 15:05:43 -0500

    improve dir in if statement

--- conf.py
+++ conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../improve/framework.py'))
+if os.path.isdir(‘../‘improve):
+    sys.path.insert(0, os.path.abspath('../improve/framework.py'))
 
 
 # -- Project information -----------------------------------------------------
