ba1ab80ba6a5ba77f0373b0e584cb2da73ab095b
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-07-02 15:02:20 -0400

    comment out problematic theme lines

--- conf.py
+++ conf.py
@@ -55,13 +55,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     'collapse_navigation': False,
     'display_version': True
 }
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
