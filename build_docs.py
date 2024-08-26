31b14f5d59c13793c25ee50bbfb7bf4f4aec27b8
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-08-26 14:50:30 -0400

    versioning stuff

---
+++ build_docs.py
@@ -0,0 +1,36 @@
+# https://www.codingwiththomas.com/blog/my-sphinx-best-practice-for-a-multiversion-documentation-in-different-languages
+
+# a single build step, which keeps conf.py and versions.yaml at the main branch
+# in generall we use environment variables to pass values to conf.py, see below
+# and runs the build as we did locally
+def build_doc(version, tag):
+    os.environ["current_version"] = version
+    subprocess.run("git checkout " + tag, shell=True)
+    subprocess.run("git checkout main -- conf.py", shell=True)
+    subprocess.run("git checkout main -- versions.yaml", shell=True)
+    subprocess.run("make html", shell=True)    
+
+# a move dir method because we run multiple builds and bring the html folders to a 
+# location which we then push to github pages
+def move_dir(src, dst):
+  subprocess.run(["mkdir", "-p", dst])
+  subprocess.run("mv "+src+'* ' + dst, shell=True)
+
+# to separate a single local build from all builds we have a flag, see conf.py
+os.environ["build_all_docs"] = str(True)
+os.environ["pages_root"] = "https://jdacs4c-improve.github.io/docs"
+
+# manually the main branch build in the current supported languages
+build_doc("latest", "main")
+move_dir("./_build/html/", "../pages/")
+
+
+# reading the yaml file
+with open("versions.yaml", "r") as yaml_file:
+  docs = yaml.safe_load(yaml_file)
+
+# and looping over all values to call our build with version, language and its tag
+for version, details in docs.items():
+  tag = details.get('tag', '') 
+  build_doc(version, tag)
+  move_dir("./_build/html/", "../pages/"+version+'/')
--- conf.py
+++ conf.py
@@ -55,15 +55,59 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     'collapse_navigation': False,
     'display_version': True
 }
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+#pages_root = "https://jdacs4c-improve.github.io/docs"
+#html_context = {'current_version' : "v0.1.0", 'versions' : [["v0.0.3-beta", pages_root+"/v0.0.3-beta/index.html"], ["v0.1.0", pages_root+"/v0.1.0-alpha/index.html"]]}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+###############
+# get the environment variable build_all_docs and pages_root
+build_all_docs = os.environ.get("build_all_docs")
+pages_root = os.environ.get("pages_root", "")
+
+# if not there, we dont call this
+if build_all_docs is not None:
+  # we get the current language and version
+  current_version = os.environ.get("current_version")
+
+  # we set the html_context wit current language and version 
+  # and empty languages and versions for now
+  html_context = {
+    'current_version' : current_version,
+    'versions' : [],
+  }
+
+
+  # and we append all versions and langauges accordingly 
+  # we treat t he main branch as latest 
+if (current_version == 'latest'):
+  #  html_context['languages'].append(['en', pages_root])
+  #  html_context['languages'].append(['de', pages_root+'/de'])
+
+  #if (current_language == 'en'):
+    html_context['versions'].append(['latest', pages_root])
+  #if (current_language == 'de'):
+  #  html_context['versions'].append(['latest', pages_root+'/de'])
+
+  # and loop over all other versions from our yaml file
+  # to set versions and languages
+with open("versions.yaml", "r") as yaml_file:
+    docs = yaml.safe_load(yaml_file)
+
+  #if (current_version != 'latest'):
+  #  for language in docs[current_version].get('languages', []):
+  #    html_context['languages'].append([language, pages_root+'/'+current_version+'/'+language])
+
+for version, details in docs.items():
+    html_context['versions'].append([version, pages_root+'/'+version+'/'])
