996362851f069da031c051365d13898837694932
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-08-27 14:36:10 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     'collapse_navigation': False,
-    'display_version': True
+    'display_version': False
 }
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
